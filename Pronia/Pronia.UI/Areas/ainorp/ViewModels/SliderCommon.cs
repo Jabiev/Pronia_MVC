2a64f2c6f86f7caf8ce9fe93cef9f34b167bb5bb
DESKTOP-1424DPE\HP <jabieviam@gmail.com>
2024-11-22 03:29:48 +0400

    completed CRUD with IFormFile

---
+++ Pronia/Pronia.UI/Areas/ainorp/ViewModels/SliderCommon.cs
@@ -0,0 +1,10 @@
+﻿using Pronia.Domain.Entities;
+
+namespace Pronia.UI.Areas.ainorp.ViewModels;
+
+public class SliderCommon
+{
+    public SliderViewModel SliderModel { get; set; }
+    public Slider Slider { get; set; }
+
+}
