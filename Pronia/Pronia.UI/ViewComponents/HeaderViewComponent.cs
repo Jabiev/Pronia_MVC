dcfe64e8cee00f19d03459f1d81dc7018426ae43
DESKTOP-1424DPE\HP <jabieviam@gmail.com>
2024-12-01 12:05:59 +0400

    added Header View Component

---
+++ Pronia/Pronia.UI/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.Data.Contexts;
+using Pronia.Domain.Entities;
+
+namespace Pronia.UI.ViewComponents;
+
+public class HeaderViewComponent(ProniaDbContext context) : ViewComponent
+{
+    private readonly ProniaDbContext _context = context;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var datas = await _context.Biography.ToDictionaryAsync(b => b.Key, b => new Biography()
+        {
+            Icon = b.Icon,
+            Value = b.Value
+        });
+        return View(datas);
+    }
+}
